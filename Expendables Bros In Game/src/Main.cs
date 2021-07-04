90d85328e87758fecdb71f00078979fc04f7c4f1
Gorzon38 <life.gorzon@gmail.com>
2021-07-04 10:59:30 +0200

    Update for v1.1.1
    V1.1.1 :
      - Add a warning text for using FilteredBros.
      - The mod does nothing if it's disable.
      - Clean up some unused line.

--- Expendables Bros In Game/src/Main.cs
+++ Expendables Bros In Game/src/Main.cs
@@ -14,7 +14,6 @@ namespace ExpendablesBrosInGame_Mod
         public static bool enabled;
         public static Settings settings;
 
-
         static Dictionary<int, HeroType> ExpendablesBro_dico = new Dictionary<int, HeroType>() {
             { 42, HeroType.BroneyRoss },
             { 52, HeroType.LeeBroxmas },
@@ -24,7 +23,6 @@ namespace ExpendablesBrosInGame_Mod
             { 92, HeroType.Broc },
             { 102, HeroType.TollBroad }
         };
-
 
         static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -41,51 +39,53 @@ namespace ExpendablesBrosInGame_Mod
             }
             catch (Exception e)
             {
-               mod.Logger.Log(e.ToString());
+                mod.Logger.Log(e.ToString());
             }
-
-
             return true;
         }
         static void OnGUI(UnityModManager.ModEntry modEntry)
         {
+            var warning = new GUIStyle();
+            warning.normal.textColor = Color.yellow;
+            warning.fontStyle = FontStyle.Bold;
             GUILayout.BeginHorizontal();
-            settings.modEnabled = GUILayout.Toggle(settings.modEnabled, "Enable Expandabros Bros", GUILayout.ExpandWidth(true));
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("If you have 'FiltredBros', i recommend you to disable this mod.", warning, GUILayout.ExpandWidth(false));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            settings.ExpendaEnable = GUILayout.Toggle(settings.ExpendaEnable, "Enable Expendabros", GUILayout.ExpandWidth(false));
             settings.brondflyEnabled = GUILayout.Toggle(settings.brondflyEnabled, "Enable Brondfly", GUILayout.ExpandWidth(false));
             GUILayout.EndHorizontal();
-
         }
-
         static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
             settings.Save(modEntry);
         }
-
         static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
         {
             enabled = value;
             return true;
         }
-        public static void Log(String str)
+        internal static void Log(object str)
         {
-            mod.Logger.Log(str);
+            mod.Logger.Log(str.ToString());
         }
 
-        public static void CheckExpendables(Dictionary<int, HeroType> Bro_dico)
+        internal static void CheckExpendables(Dictionary<int, HeroType> Bro_dico)
         {
             try
             {
                 int brondflyStep = 861;
-                //Add Expendabable Bros
+                //Add Expendable Bros
                 foreach (KeyValuePair<int, HeroType> bros in ExpendablesBro_dico)
                 {
-                    if (!settings.modEnabled & Bro_dico.Contains(bros))
+                    if (!settings.ExpendaEnable & Bro_dico.Contains(bros))
                     {
                         Main.Log("Remove " + bros.Value + ".....");
                         Bro_dico.Remove(bros.Key);
                     }
-
-                    else if (settings.modEnabled & !Bro_dico.Contains(bros))
+                    else if (settings.ExpendaEnable & !Bro_dico.Contains(bros))
                     {
                         if (bros.Value == HeroType.BrondleFly & settings.brondflyEnabled)
                             Main.Log(bros.Value + " is missing ! Adding....");
@@ -93,7 +93,7 @@ namespace ExpendablesBrosInGame_Mod
                     }
                 }
 
-                //Add or remove brondfly depend on option
+                //Add or remove Brondfly depend on option
                 if (settings.brondflyEnabled & !Bro_dico.ContainsValue(HeroType.BrondleFly))
                 {
                     Main.Log("Brondfly is missing ! Adding....");
@@ -104,10 +104,8 @@ namespace ExpendablesBrosInGame_Mod
                     Main.Log("Remove Brondfly.....");
                     Bro_dico.Remove(brondflyStep);
                 }
-
             }
-            catch (Exception ex) { Main.Log(ex.ToString()); }
-
+            catch (Exception ex) { Main.Log(ex); }
         }
     }
 
@@ -115,25 +113,22 @@ namespace ExpendablesBrosInGame_Mod
     static class HeroUnlockController_IsAvailableInCampaign_Patch
     {
         // After my mod FiltredBros, i have no idea how this can working..
-            public static bool Prefix(ref HeroType hero)
-            {
-                Dictionary<int, HeroType> newHeroUnlockIntervals = Traverse.Create(typeof(HeroUnlockController)).Field("_heroUnlockIntervals").GetValue() as Dictionary<int, HeroType>;
+        public static bool Prefix(ref HeroType hero)
+        {
+            Dictionary<int, HeroType> newHeroUnlockIntervals = Traverse.Create(typeof(HeroUnlockController)).Field("_heroUnlockIntervals").GetValue() as Dictionary<int, HeroType>;
+            if(Main.enabled)
                 Main.CheckExpendables(newHeroUnlockIntervals);
-                return newHeroUnlockIntervals.ContainsValue(hero);
-            }
+            return newHeroUnlockIntervals.ContainsValue(hero);
+        }
     }
 
     public class Settings : UnityModManager.ModSettings
     {
-        public bool modEnabled;
         public bool brondflyEnabled;
-
+        public bool ExpendaEnable;
         public override void Save(UnityModManager.ModEntry modEntry)
         {
             Save(this, modEntry);
         }
-
     }
 }
-
-
