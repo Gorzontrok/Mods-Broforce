351d1ee14e717f28b539713256c187f15a6112b3
Gorzon38 <life.gorzon@gmail.com>
2021-07-05 12:16:04 +0200

    Create ModTemplate.cs
    I copy-cut when i create a new mod, it's faster than write all.

---
+++ ModTemplate.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace ModTemplate
+{
+    static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        public static bool enabled;
+        public static Settings settings;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            modEntry.OnToggle = OnToggle;
+            settings = Settings.Load<Settings>(modEntry);
+            var harmony = new Harmony(modEntry.Info.Id);
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                harmony.PatchAll(assembly);
+            }
+            catch(Exception ex)
+            {
+                mod.Logger.Log(ex.ToString());
+            }
+
+            mod = modEntry;
+
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+
+        public static void Log(object str)
+        {
+            mod.Logger.Log(str.ToString());
+        }
+
+    }
+
+    public class Settings : UnityModManager.ModSettings
+    {
+        public int count;
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+
+    }
+}
