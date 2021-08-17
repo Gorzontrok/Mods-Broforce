43212e55ebb41a30561e2a07263a2d3f7bbb6ce7
Gorzon38 <life.gorzon@gmail.com>
2021-08-17 10:37:40 +0200

    Add CustomHeroName

---
+++ .Unreleased/Custom Hero Name/src/Main.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace CustomHeroName
+{
+using RocketLib;
+    static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        public static bool enabled;
+        public static Settings settings;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            modEntry.OnToggle = OnToggle;
+
+            settings = Settings.Load<Settings>(modEntry);
+            try
+            {
+                var harmony = new Harmony(modEntry.Info.Id);
+                var assembly = Assembly.GetExecutingAssembly();
+                harmony.PatchAll(assembly);
+            }
+            catch (Exception ex)
+            {
+                mod.Logger.Log("Failed to patch with Harmony !\n" + ex.ToString());
+            }
+
+
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("View name"))
+            {
+                foreach(HeroType hero in RocketLib._HeroUnlockController.HeroTypeFullList)
+                    Main.Log(GetBasicNameInTxt(hero)+"\t"+GetNewName(hero));
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+
+        public static void Log(object str)
+        {
+            mod.Logger.Log(str.ToString());
+        }
+
+        static string GetBasicNameInTxt(HeroType Hero)
+        {
+            switch (Hero)
+            {
+                case HeroType.AshBrolliams: return "AshBrolliams";
+                case HeroType.BaBroracus: return "B.A.Broracus";
+                case HeroType.Blade: return "Brade";
+                case HeroType.BoondockBros: return "BoondockBros";
+                case HeroType.Brobocop: return "Brobocop";
+                case HeroType.Brochete: return "Brochete";
+                case HeroType.BrodellWalker: return "BrodellWalker";
+                case HeroType.Broden: return "Broden";
+                case HeroType.BroDredd: return "BroDredd";
+                case HeroType.BroHard: return "BroHard";
+                case HeroType.BroLee: return "BroLee";
+                case HeroType.BroMax: return "BroMax";
+                case HeroType.Brominator: return "Brominator";
+                case HeroType.Brommando: return "Brommando";
+                case HeroType.BronanTheBrobarian: return "BronanTheBrobarian";
+                case HeroType.BroniversalSoldier: return "BroniversalSoldier";
+                case HeroType.Brononymous: return "BroInBlack";
+                case HeroType.BroveHeart: return "BroHeart";
+                case HeroType.CherryBroling: return "CherryBroling";
+                case HeroType.ColJamesBroddock: return "Col.JamesBroddock";
+                case HeroType.DirtyHarry: return "DirtyBrody";
+                case HeroType.DoubleBroSeven: return "DoubleBroSeven";
+                case HeroType.EllenRipbro: return "EllenRipbro";
+                case HeroType.IndianaBrones: return "IndianaBrones";
+                case HeroType.McBrover: return "MacBrover";
+                case HeroType.Nebro: return "MrAnderbro";
+                case HeroType.Predabro: return "TheBrodator";
+                case HeroType.Rambro: return "Rambro";
+                case HeroType.SnakeBroSkin: return "SnakeBroSkin";
+                case HeroType.TankBro: return "TankBro";
+                case HeroType.TheBrocketeer: return "TheBrocketeer";
+                case HeroType.TheBrode: return "TheBrode";
+                case HeroType.TheBrofessional: return "TheBrofessional";
+                case HeroType.TheBrolander: return "TheBrolander";
+                case HeroType.TimeBroVanDamme: return "TimeBro";
+
+                //Expendabros
+                case HeroType.Broc: return "BroctorDeath";
+                case HeroType.BroneyRoss: return "BroneyRoss";
+                case HeroType.BronnarJensen: return "BronnarJensen";
+                case HeroType.HaleTheBro: return "BroCaesar";
+                case HeroType.LeeBroxmas: return "LeeBroxmas";
+                case HeroType.TollBroad: return "TollBroad";
+                case HeroType.TrentBroser: return "TrentBroser";
+
+                case HeroType.BrondleFly: return "BrondleFly";
+            }
+            return "";
+        }
+
+        public static string GetBasicNameOnNull(HeroType type)
+        {
+            switch (type)
+            {
+                case HeroType.BaBroracus:
+                    return "B.A. Broracus";
+                case HeroType.BrodellWalker:
+                    return "Brodell Walker";
+                case HeroType.Blade:
+                    return "Brade";
+                case HeroType.McBrover:
+                    return "MacBrover";
+                case HeroType.Brononymous:
+                    return "Bro In Black";
+                case HeroType.BroDredd:
+                    return "Bro Dredd";
+                case HeroType.BroHard:
+                    return "Bro Hard";
+                case HeroType.SnakeBroSkin:
+                    return "Snake Broskin";
+                case HeroType.IndianaBrones:
+                    return "Indiana Brones";
+                case HeroType.AshBrolliams:
+                    return "Ash Brolliams";
+                case HeroType.Nebro:
+                    return "Mr Anderbro";
+                case HeroType.BoondockBros:
+                    return "Boondock Bros";
+                case HeroType.BronanTheBrobarian:
+                    return "Bronan The Brobarian";
+                case HeroType.EllenRipbro:
+                    return "Ellen Ripbro";
+                case HeroType.CherryBroling:
+                    return "Cherry Broling";
+                case HeroType.TimeBroVanDamme:
+                    return "Time Bro";
+                case HeroType.ColJamesBroddock:
+                    return "Col. James Broddock";
+                case HeroType.BroniversalSoldier:
+                    return "Broniversal Soldier";
+                case HeroType.BroneyRoss:
+                    return "Broney Ross";
+                case HeroType.LeeBroxmas:
+                    return "Lee Broxmas";
+                case HeroType.BronnarJensen:
+                    return "Bronnar Jensen";
+                case HeroType.HaleTheBro:
+                    return "Bro Caesar";
+                case HeroType.TrentBroser:
+                    return "Trent Broser";
+                case HeroType.Broc:
+                    return "Broctor Death";
+                case HeroType.TollBroad:
+                    return "Toll Broad";
+                case HeroType.TheBrode:
+                    return "The Brode";
+                case HeroType.BroMax:
+                    return "Bro Max";
+                case HeroType.DoubleBroSeven:
+                    return "Double Bro Seven";
+                case HeroType.Predabro:
+                    return "The Brodator";
+                case HeroType.TheBrofessional:
+                    return "The Brofessional";
+                case HeroType.BrondleFly:
+                    return "Brondle Fly";
+                case HeroType.BroveHeart:
+                    return "Bro Heart";
+                case HeroType.TheBrocketeer:
+                    return "The Brocketeer";
+                case HeroType.TankBro:
+                    return "Tank Bro";
+                case HeroType.TheBrolander:
+                    return "The Brolander";
+                case HeroType.BroLee:
+                    return "Bro Lee";
+                case HeroType.DirtyHarry:
+                    return "Dirty Brody";
+            }
+            return type.ToString();
+        }
+
+        public static string GetNewName(HeroType Hero)
+        {
+            string NameToReturn = "";
+
+            string[] lines = File.ReadAllLines(mod.Path + "CustomHeroName.txt");
+            string TxtBasicName = GetBasicNameInTxt(Hero);
+
+            foreach(string line in lines)
+            {
+                string[] words = line.Split();
+                if(words.Length > 1)
+                {
+                    if(words[0] == TxtBasicName)
+                    {
+                        for (int i = 1; i < words.Length; i++)
+                        {
+                            NameToReturn += words[i] + " ";
+                        }
+                    }
+                }
+            }
+            //Main.Log(NameToReturn);
+            return NameToReturn;
+        }
+
+    }
+
+    public class Settings : UnityModManager.ModSettings
+    {
+        public int FileSelect;
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+
+    }
+    
+    [HarmonyPatch(typeof(HeroController), "GetHeroName")]
+    static class CustomName_Patch
+    {
+        public static void Postfix(ref HeroType type, ref string __result)
+        {
+            if (Main.enabled)
+            {
+                try
+                {
+                    __result = Main.GetNewName(type);
+
+                    foreach(HeroType hero in RocketLib._HeroUnlockController.HeroTypeFullList)
+                    {
+                        if(Main.GetBasicNameOnNull(hero) == __result)
+                        {
+                            __result = Main.GetNewName(hero);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Main.Log(ex);
+                }
+            }
+
+            if (String.IsNullOrEmpty(__result)) __result = Main.GetBasicNameOnNull(type);
+
+        }
+    }
+}
---
+++ .Unreleased/ForBralef/src/Main.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace ForBralef
+{
+    static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        public static bool enabled;
+        public static Settings settings;
+
+        public static List<HeroType> origHeroList = new List<HeroType> { HeroType.AshBrolliams, HeroType.BaBroracus, HeroType.Blade, HeroType.BoondockBros, HeroType.Brobocop, HeroType.Broc, HeroType.Brochete, HeroType.BrodellWalker, HeroType.Broden, HeroType.BroDredd, HeroType.BroHard, HeroType.BroLee, HeroType.BroMax, HeroType.Brominator, HeroType.Brommando, HeroType.BronanTheBrobarian, HeroType.BrondleFly, HeroType.BroneyRoss, HeroType.BroniversalSoldier, HeroType.BronnarJensen, HeroType.Brononymous, HeroType.BroveHeart, HeroType.CherryBroling, HeroType.ColJamesBroddock, HeroType.DirtyHarry, HeroType.DoubleBroSeven, HeroType.EllenRipbro, HeroType.HaleTheBro, HeroType.IndianaBrones, HeroType.LeeBroxmas, HeroType.McBrover, HeroType.Nebro, HeroType.Predabro, HeroType.Rambro, HeroType.SnakeBroSkin, HeroType.TankBro, HeroType.TheBrocketeer, HeroType.TheBrode, HeroType.TheBrofessional, HeroType.TheBrolander, HeroType.TimeBroVanDamme, HeroType.TollBroad, HeroType.TrentBroser };
+        
+        public static List<HeroType> HeroList = new List<HeroType> { HeroType.AshBrolliams, HeroType.BaBroracus, HeroType.Blade, HeroType.BoondockBros, HeroType.Brobocop, HeroType.Broc, HeroType.Brochete, HeroType.BrodellWalker, HeroType.Broden, HeroType.BroDredd, HeroType.BroHard, HeroType.BroLee, HeroType.BroMax, HeroType.Brominator, HeroType.Brommando, HeroType.BronanTheBrobarian, HeroType.BrondleFly, HeroType.BroneyRoss, HeroType.BroniversalSoldier, HeroType.BronnarJensen, HeroType.Brononymous, HeroType.BroveHeart, HeroType.CherryBroling, HeroType.ColJamesBroddock, HeroType.DirtyHarry, HeroType.DoubleBroSeven, HeroType.EllenRipbro, HeroType.HaleTheBro, HeroType.IndianaBrones, HeroType.LeeBroxmas, HeroType.McBrover, HeroType.Nebro, HeroType.Predabro, HeroType.Rambro, HeroType.SnakeBroSkin, HeroType.TankBro, HeroType.TheBrocketeer, HeroType.TheBrode, HeroType.TheBrofessional, HeroType.TheBrolander, HeroType.TimeBroVanDamme, HeroType.TollBroad, HeroType.TrentBroser };
+        public static List<int> HeroInt = new List<int> { 1, 3, 5, 8, 11, 15, 20, 25, 37, 42, 46, 52, 56, 62, 65, 72, 75, 82, 87, 92, 99, 102, 115, 123, 132, 145, 160, 175, 193, 209, 222, 249, 274, 300, 326, 350, 374, 400, 425, 445, 465, 485, 520 };
+        public static Dictionary<int, HeroType> HeroDictionary = new Dictionary<int, HeroType>();
+
+        public static bool NeedToRedoneDictionary = false;
+        public static int CurrentNbrOfBro;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
+            settings = Settings.Load<Settings>(modEntry);
+            
+            mod = modEntry;
+
+            if (!settings.GetFirstLaunch) FirstLaunch();
+            CurrentNbrOfBro = settings.MaxNumberOfBro;
+
+            if (settings.RememberList) { HeroList = settings.HeroListSave; BuildTheDictionary(); }
+
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            var LabelWarning = new GUIStyle();
+            LabelWarning.normal.textColor = Color.yellow;
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Unlock all")) HeroUnlockController.UnlockAllBros();
+            if (GUILayout.Button("Clear Unlock")) HeroUnlockController.ClearUnlocks();
+            GUILayout.Space(5);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Max number of bro : " + settings.MaxNumberOfBro.ToString(), GUILayout.ExpandWidth(false));
+            settings.MaxNumberOfBro = (int)GUILayout.HorizontalScrollbar(settings.MaxNumberOfBro, 1f, 1f, origHeroList.Count + 1);
+            GUILayout.Space(50);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Shuffle dictionnary")) ShuffleList();
+
+            if (GUILayout.Button("Show the current bros order in log")) ShowCurrentDictionnary();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.Space(10);
+            if (NeedToRedoneDictionary) GUILayout.Label("NEED TO BUILD DICTIONARY", LabelWarning);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Build Dictionary")) BuildTheDictionary();
+            settings.RememberList = GUILayout.Toggle(settings.RememberList, "Remember dictionary ?");
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+
+        static void OnUpdate(UnityModManager.ModEntry modEntry, float dt)
+        {
+            if (settings.MaxNumberOfBro != CurrentNbrOfBro) NeedToRedoneDictionary = true;
+            if (settings.MaxNumberOfBro == CurrentNbrOfBro) NeedToRedoneDictionary = false;
+
+            if (!settings.RememberList) settings.HeroListSave = new List<HeroType>();
+            if (settings.RememberList) settings.HeroListSave = HeroList;
+            
+        }
+        
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            enabled = value;
+            return true;
+        }
+
+        public static void Log(object str)
+        {
+            mod.Logger.Log(str.ToString());
+        }
+
+        static void FirstLaunch()
+        {
+            settings.MaxNumberOfBro = HeroList.Count;
+            settings.GetFirstLaunch = true;
+        }
+
+        static void ShuffleList() 
+        { 
+            HeroList.Shuffle();
+            Main.Log("List shuffle ! Build dictionary know !!");
+            NeedToRedoneDictionary = true;
+        }
+
+        static void BuildTheDictionary()
+        {
+            HeroDictionary.Clear();
+            CurrentNbrOfBro = settings.MaxNumberOfBro;
+            int i = 0;
+            try
+            {
+                foreach (HeroType Hero in HeroList)
+                {
+                    if(i < settings.MaxNumberOfBro)
+                    {
+                        HeroDictionary.Add(HeroInt[i], Hero);
+                        i++;
+                    }
+                }
+            }
+            catch (Exception ex) { Main.Log("Failed to build Dictionnary !\n\t" + ex); return; }
+            Main.Log("Finish to create Dictionary.");
+
+            try
+            {
+                Traverse.Create(typeof(HeroUnlockController)).Field("_heroUnlockIntervals").SetValue(HeroDictionary);
+            }
+            catch (Exception ex) { Main.Log("Failed to assign dictionnary ! \n\t" + ex); return; }
+
+            NeedToRedoneDictionary = false;
+        }
+        
+        static void ShowCurrentDictionnary()
+        {
+            if (HeroDictionary.Count < 1)
+            {
+                Main.Log("No shuffle dictionnary, show the one from the game !");
+            }
+            Dictionary<int, HeroType> HeroUnlockIntervals = Traverse.Create(typeof(HeroUnlockController)).Field("_heroUnlockIntervals").GetValue() as Dictionary<int, HeroType>;
+
+            Main.Log("Start to show intervals....");
+            string Table = "\n\n int, bro\n";
+            foreach (KeyValuePair<int, HeroType> Hero in HeroUnlockIntervals)
+            {
+                Table += (" " + Hero.Key + ",  " + HeroController.GetHeroName(Hero.Value)+"\n");
+            }
+            Main.Log(Table);
+            Main.Log("Finish to show unlock order");
+        }
+
+        // Add this to RocketLib
+        private static System.Random rng = new System.Random();
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+
+    public class Settings : UnityModManager.ModSettings
+    {
+        public int MaxNumberOfBro;
+        public bool GetFirstLaunch;
+
+        public bool RememberList;
+        public List<HeroType> HeroListSave;
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+}
