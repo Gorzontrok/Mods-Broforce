5d68c33bd3874210bcd471952a5ccd519d8edd95
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-18 14:25:01 +0200

    Upload mod

---
+++ SkipIntro/README.md
@@ -0,0 +1,13 @@
+# Skip Intro
+
+[NexusMods Page](https://www.nexusmods.com/broforce/mods/5)  
+Skip the introductions of the game
+
+## Installation
+
+* UnityModManger =< 0.27.6
+* The all and [latest](./Release/SkipIntroMod.zip) release are in the [Release](./Release/) folder.
+
+## Know Issue
+
+* Gui is bugged sometimes
---
+++ SkipIntro/src/Main.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Reflection;
+using HarmonyLib;
+using UnityModManagerNet;
+
+namespace SkipIntroMod
+{
+    static class Main
+    {
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                var harmony = new Harmony(modEntry.Info.Id);
+                var assembly = Assembly.GetExecutingAssembly();
+                harmony.PatchAll(assembly);
+            }
+            catch (Exception ex)
+            {
+                modEntry.Logger.Log("Failed to Patch Harmony !\n" + ex.ToString());
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(Startup), "Update")]
+    static class SkipIntro_Patch
+    {
+        static bool Prefix(Startup __instance)
+        {
+            GameState.LoadLevel("MainMenu");
+            return false;
+        }
+    }
+}
