df0b0ab2814fcfce982c3e7b54d4b186c090575e
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 09:59:57 +0200

    Create TypeExtensions.cs

---
+++ RocketLib/src/RocketLib/Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RocketLib
+{
+    public static class TypeExtensions
+    {
+        public static bool CanBeNull(this Type type)
+        {
+            return !type.IsValueType || (Nullable.GetUnderlyingType(type) != null);
+        }
+    }
+}
