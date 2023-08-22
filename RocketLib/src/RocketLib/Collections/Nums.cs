e7efab0c3bf4b7930b69d860a6774d4bd5631fc4
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 09:53:55 +0200

    Create Nums.cs

---
+++ RocketLib/src/RocketLib/Collections/Nums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RocketLib.Collections
+{
+    public static class Nums
+    {
+        public const int TERRORIST = -1;
+        public const int ALIENS = -2;
+        public const int PLAYER_MIN = 0;
+        public const int PLAYER_MAX = 4;
+        public const int VILLAGER = 5;
+    }
+}
