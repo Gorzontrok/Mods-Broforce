71311af73bc4982da30b8cbff786e8d1ad01bf48
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 09:59:50 +0200

    Create PlayerHUDExtensions.cs

---
+++ RocketLib/src/RocketLib/Extensions/PlayerHUDExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TFBGames.Systems;
+using UnityEngine;
+
+namespace RocketLib.Extensions
+{
+    public static class PlayerHUDExtensions
+    {
+        public static void SetGrenadesMaterials(this PlayerHUD playerHUD, Material[] materials)
+        {
+            Material grenadeIcon = null;
+            for (int i = 0; i < playerHUD.grenadeIcons.Length; i++)
+            {
+
+                if (i >= materials.Length)
+                {
+                    if (grenadeIcon == null)
+                        grenadeIcon = GameSystems.ResourceManager.LoadAssetSync<Material>("sharedtextures:GrenadeIcon");
+                    playerHUD.grenadeIcons[i].GetComponent<Renderer>().material = grenadeIcon;
+                }
+                else
+                    playerHUD.grenadeIcons[i].GetComponent<Renderer>().material = materials[i];
+            }
+        }
+    }
+}
