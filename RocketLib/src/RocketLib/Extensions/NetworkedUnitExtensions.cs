7ff4ecb459285c0620978d0a692691a67a587672
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2023-08-22 09:59:00 +0200

    Create NetworkedUnitExtensions.cs

---
+++ RocketLib/src/RocketLib/Extensions/NetworkedUnitExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using RocketLib.Collections;
+
+namespace RocketLib
+{
+    public static class NetworkedUnitExtensions
+    {
+        public static bool IsPlayer(this NetworkedUnit self)
+        {
+            return (self as TestVanDammeAnim) != null && self.As<TestVanDammeAnim>().player != null && self.playerNum >= Nums.PLAYER_MIN && self.playerNum < Nums.PLAYER_MAX;
+        }
+
+        public static bool IsPlayerSide(this NetworkedUnit self)
+        {
+            return self.playerNum >= Nums.PLAYER_MIN;
+        }
+
+        public static bool IsPlayerSideAndNotPlayer(this NetworkedUnit self)
+        {
+            return self.playerNum >= Nums.PLAYER_MAX;
+        }
+
+        public static bool IsTerrorist(this NetworkedUnit self)
+        {
+            return self.playerNum == Nums.TERRORIST && self as TestVanDammeAnim;
+        }
+
+        public static bool IsAlien(this NetworkedUnit self)
+        {
+            return self.playerNum == Nums.ALIENS && self as Alien;
+        }
+
+        public static bool IsEnemy(this NetworkedUnit self)
+        {
+            return self.IsEnemy && (IsTerrorist(self) || IsAlien(self));
+        }
+    }
+}
