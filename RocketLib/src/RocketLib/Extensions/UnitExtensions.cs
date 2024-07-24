9e2958386fe3141f5213d4609b11e291f6b52848
Gorzon38 <64686176+Gorzontrok@users.noreply.github.com>
2024-07-24 14:29:48 +0200

    Create UnitExtensions.cs

---
+++ RocketLib/src/RocketLib/Extensions/UnitExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace RocketLib
+{
+    public static class UnitExtensions
+    {
+        public static void SetIsZombie(this Unit unit, bool isZombie)
+        {
+            unit.SetFieldValue("isZombie", isZombie);
+        }
+    }
+}
