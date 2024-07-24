c03906abe8ec7ed6a2209c4852b3ba46d96157e9
Gorzontrok <64686176+Gorzontrok@users.noreply.github.com>
2024-07-24 19:44:53 +0200

    Added ColorJsonConverter

---
+++ RocketLib/src/RocketLib/JsonConverters/ColorJsonConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using UnityEngine;
+
+namespace RocketLib.JsonConverters
+{
+    public class ColorJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            if (objectType == typeof(Color))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            object obj = serializer.Deserialize(reader);
+            Color color = JsonConvert.DeserializeObject<Color>(obj.ToString());
+            return color;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            Color r = (Color)value;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("r");
+            writer.WriteValue(r.r);
+            writer.WritePropertyName("g");
+            writer.WriteValue(r.g);
+            writer.WritePropertyName("b");
+            writer.WriteValue(r.b);
+            writer.WritePropertyName("a");
+            writer.WriteValue(r.a);
+            writer.WriteEndObject();
+        }
+    }
+}
